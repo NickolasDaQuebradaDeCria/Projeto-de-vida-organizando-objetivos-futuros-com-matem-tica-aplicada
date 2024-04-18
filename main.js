c6da71a1a23f6fa9d88af7e0b7ff6d7c00f3a2d0
Nickolas <110051300+NickolasDaQuebradaDeCria@users.noreply.github.com>
2024-04-18 16:20:13 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 const botoes = document.querySelectorAll(".botao");
 
 for(let i=0;i <botoes.length;i++){
-    console.log();
+    botoes[i].onclick = function(){
+
+        for(let j=0;j<botoes.length;j++){
+            botoes[j].classList.remove("ativo");
+        }
+
+        botoes[i].classList.add("ativo");
+    }
+}
+var frutas = ["Maçã", "Banana", "Laranja", "Pera"];
+for (var i = 0; i < frutas.length; i++) {
+  console.log(frutas[i]);
 }
